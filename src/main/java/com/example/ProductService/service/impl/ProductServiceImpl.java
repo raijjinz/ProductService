ea73d705f753b7d2008c388d9e937da8838189c0
Chaitanya Jain <chaitanyajain@Chaitanyas-MacBook-Pro.local>
2021-03-08 11:13:53 +0530

    beforeSubmitS1

--- src/main/java/com/example/ProductService/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/ProductService/service/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.ProductService.service.impl;
 
+import com.example.ProductService.dto.ProductPostDTO;
 import com.example.ProductService.dto.ProductRequestDTO;
 import com.example.ProductService.dto.ProductResponseDTO;
 import com.example.ProductService.entity.Product;
@@ -7,6 +8,7 @@ import com.example.ProductService.repository.ProductRepository;
 import com.example.ProductService.service.ProductService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -19,20 +21,26 @@ public class ProductServiceImpl implements ProductService {
 
 
     @Override
-    public ArrayList<ProductResponseDTO> getProductsByPids(ArrayList<Long> arrOfP){
-        ArrayList<ProductResponseDTO> ResponseArr = new ArrayList<>();
-        arrOfP.forEach((id) -> {
+    public List<Product> getProductsByPids(ArrayList<Long> arrOfP){
+//        ArrayList<ProductResponseDTO> ResponseArr = new ArrayList<>();
+//        arrOfP.forEach((id) -> {
+//
+//            Optional<Product> productOptional = productRepository.findById(id);
+//            if (productOptional.isPresent()) {
+//                ProductResponseDTO productResponseDTO = new ProductResponseDTO();
+//                BeanUtils.copyProperties(productOptional.get(), productResponseDTO);
+//                ResponseArr.add(productResponseDTO);
+//                //return productResponseDTO;
+//            }
+//        });
 
-            Optional<Product> productOptional = productRepository.findById(id);
-            if (productOptional.isPresent()) {
-                ProductResponseDTO productResponseDTO = new ProductResponseDTO();
-                BeanUtils.copyProperties(productOptional.get(), productResponseDTO);
-                ResponseArr.add(productResponseDTO);
-                //return productResponseDTO;
-            }
-        });
+//        return ResponseArr;
+        List<Product> QueryArr = productRepository.queryProducts(arrOfP);
 
-        return ResponseArr;
+        System.out.println(QueryArr);
+        return QueryArr;
+
+
 
         }
 
@@ -55,7 +63,22 @@ public class ProductServiceImpl implements ProductService {
 
      }
 
+
+    @Override
+    public ProductResponseDTO addProducts(ProductPostDTO requestDTO)
+    {
+
+        Product product = new Product();
+        BeanUtils.copyProperties(requestDTO, product);
+        Product savedProduct = productRepository.save(product);
+        ProductResponseDTO productResponseDTO = new ProductResponseDTO();
+        BeanUtils.copyProperties(savedProduct, productResponseDTO);
+
+        return productResponseDTO;
+
+
     }
+}
 
 
 
